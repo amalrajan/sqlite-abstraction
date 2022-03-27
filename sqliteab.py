8b8f59c6db2436e29eb68695dc50c858f72694aa
Amal Rajan <amalrajan74@gmail.com>
2022-03-27 21:27:22 +0000

    Initial commit

---
+++ sqliteab.py
@@ -0,0 +1,196 @@
+import logging
+import sqlite3
+
+
+class SQLiteAb:
+    """
+    A quick & dirty abstraction layer written for SQLite databases.
+
+    Do not use this for actual work
+    """
+
+    def __init__(self, database) -> None:
+        self._connection = sqlite3.connect(database)
+        self._cursor = self._connection.cursor()
+        self._table = None
+
+    def __xi(self, text):
+        return f'{text}' if f'{text}'.isdigit() else f"'{text}'"
+
+    def enable_logging(self):
+        """
+        Enable console logging
+        """
+        logging.basicConfig(
+            level=logging.DEBUG,
+            format='%(process)d-%(levelname)s-%(message)s'
+        )
+
+    def set_table(self, table) -> None:
+        """
+        Sets the current table
+
+        Args:
+            table (string): _description_
+        """
+        self._table = table
+
+    def get_all_data(self):
+        """
+        Gets all data as a tuple
+
+        Returns:
+            tuple: _description_
+        """
+        query = f'''
+            SELECT * FROM {self._table}
+        '''
+
+        res = self._cursor.execute(query)
+
+        return res
+
+    def get_data(self, column_name, column_val):
+        """
+        Get a tuple of rows matching a given condition
+
+        Args:
+            column_name (_type_): _description_
+            column_val (_type_): _description_
+
+        Returns:
+            _type_: _description_
+        """
+        query = f'''
+            SELECT * FROM {self._table}
+            WHERE {column_name} = {self.__xi(column_val)}
+        '''
+
+        logging.debug(query)
+
+        res = self._cursor.execute(query)
+
+        return res
+
+    def table_exists(self):
+        """
+        Checks if a table exists
+
+        Returns:
+            Boolean: _description_
+        """
+        query = f'''
+            SELECT 1 FROM {self._table}
+        '''
+
+        res = self._cursor.execute(query)
+
+        return True if res else False
+
+    def create_table(self, mp):
+        """
+        Create a table
+
+        Args:
+            mp (column-name -> column-type): _description_
+        """
+
+        query = f'''
+            CREATE TABLE {self._table} ({', '.join([
+                f"{i} {mp[i]}" for i in mp
+            ])})
+        '''
+
+        logging.debug(query)
+
+        self._cursor.execute(query)
+        self._connection.commit()
+
+    def insert_row(self, mp):
+        """
+        Inserts a row into a table
+
+        Args:
+            mp (map column-name -> data): _description_
+        """
+        map_keys = mp.keys()
+
+        query = f'''
+            INSERT INTO {self._table}
+            ({', '.join(map_keys)})
+            VALUES
+            ({', '.join(
+                [self.__xi(mp[i]) for i in map_keys]
+            )})
+        '''
+
+        logging.debug(query)
+
+        self._cursor.execute(query)
+        self._connection.commit()
+
+    def truncate_table(self):
+        """
+        Truncate the table
+        """
+        query = f'DELETE FROM {self._table}'
+
+        self._cursor.execute(query)
+        self._connection.commit()
+
+    def delete_row(self, column_name, column_val):
+        """
+        Delete rows based on condition
+
+        Args:
+            column_name (string): _description_
+            column_val (string or integer): _description_
+        """
+        query = f'''
+            DELETE FROM {self._table}
+            WHERE {column_name} = {self.__xi(column_val)}
+        '''
+
+        logging.debug(query)
+
+        self._cursor.execute(query)
+        self._connection.commit()
+
+    def modify_row(self, column_name, column_val, mp):
+        """
+        Updates a single row
+
+        Args:
+            column_name (string): _description_
+            column_val (string): _description_
+            mp (map column-name -> new data): _description_
+        """
+        updates = ''
+        for k in mp:
+            updates += f'{k}={self.__xi(mp[k])},'
+
+        query = f'''
+            UPDATE {self._table} 
+            SET {updates[:-1]}
+            WHERE {column_name} = {self.__xi(column_val)}
+        '''
+
+        logging.debug(query)
+
+        self._cursor.execute(query)
+        self._connection.commit()
+
+    def drop_table(self):
+        """
+        Drop a table
+        """
+        query = f'DROP TABLE {self._table}'
+
+        self._cursor.execute(query)
+        self._connection.commit()
+
+    def close_connection(self):
+        """
+        Close SQLite connection
+        """
+        self._connection.close()
